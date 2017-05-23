844a9396755d82af23287d6960de3ba6bdd62828
xingbofeng <503908971@qq.com>
2017-05-23 19:44:47 +0800

    chore:ok_hand::采用分支管理python版本和node版本

--- python/deployment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 指定脚本版本,然后进行部署.
-
-rm -rf ./dist/*
-
-# 以下所有生成文件将在当前路径下 dist 目录中
-python3 setup.py bdist_egg # 生成easy_install支持的格式
-python3 setup.py sdist     # 生成pip支持的格式，下文以此为例
-python setup.py sdist upload
